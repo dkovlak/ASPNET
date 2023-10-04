fbcbdd6bc38ffbd52a3547dfa6131d6ccfb4d348
David Kovlak <kovlakdavid4@gmail.com>
2023-10-04 16:02:38 -0700

    Completed my first basic CRUD ASP.NET Core MVC Web Application

--- Testing/Models/IProductRepository.cs
+++ Testing/Models/IProductRepository.cs
@@ -14,6 +14,8 @@ namespace Testing.Models
 		public void InsertProduct(Product productToInsert);
 		public IEnumerable<Category> GetCategories();
 		public Product AssignCategory();
+
+		public void DeleteProduct(Product product);
 		
 	}
 }
